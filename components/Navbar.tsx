e5c8577ca7a88760c37e67651c20cff8bc261df8
GulenkovaNataliya <gulenkovanatalia@gmail.com>
2026-03-02 19:19:16 +0200

    increase navbar logo size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,15 @@
 export default function Navbar() {
   return (
-    <nav className="absolute top-0 left-0 z-20 w-full px-6 py-4">
-      <a href="/" className="flex items-center">
-        <img
-          src="/logo/logo-main.png"
-          alt="1Choice"
-          className="w-[230px] h-auto"
-        />
-      </a>
+    <nav className="absolute top-0 left-0 z-20 w-full">
+      <div className="mx-auto flex h-28 max-w-6xl items-center justify-between px-4">
+        <a href="/" className="flex items-center">
+          <img
+            src="/logo/logo-main.png"
+            alt="1Choice"
+            className="w-[240px] h-auto"
+          />
+        </a>
+      </div>
     </nav>
   );
 }

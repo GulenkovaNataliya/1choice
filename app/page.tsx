1bef9bcc52a5f683b03afba22c9bc8890577b8aa
GulenkovaNataliya <gulenkovanatalia@gmail.com>
2026-03-02 22:46:18 +0200

    refactor: remove featured/why/gv sections from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import HeroVideo from "@/components/HeroVideo";
 import HorizontalFilter from "@/components/Home/HorizontalFilter";
 import HomeNavButtons from "@/components/Home/HomeNavButtons";
-import FeaturedProperties from "@/components/Home/FeaturedProperties";
-import WhyWorkWithUs from "@/components/Home/WhyWorkWithUs";
-import InvestmentGoldenVisaTeaser from "@/components/Home/InvestmentGoldenVisaTeaser";
 import Footer from "@/components/Layout/Footer";
 
 export default function Home() {
@@ -12,9 +9,6 @@ export default function Home() {
       <HeroVideo />
       <HorizontalFilter />
       <HomeNavButtons />
-      <FeaturedProperties />
-      <WhyWorkWithUs />
-      <InvestmentGoldenVisaTeaser />
       <Footer />
     </>
   );

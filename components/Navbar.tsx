59af34f83869c300e8598cfd80b5859619b8407f
GulenkovaNataliya <gulenkovanatalia@gmail.com>
2026-03-02 19:50:24 +0200

    replace navbar logo with updated version

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
   return (
     <nav className="absolute top-0 left-0 z-20 w-full">
-      <div className="mx-auto flex h-28 max-w-6xl items-center justify-between px-4">
-        <a href="/" className="flex items-center">
+      <div className="pl-24 pt-12">
+        <a href="/">
           <img
             src="/logo/logo-main.png"
             alt="1Choice"
-            className="w-[280px] h-auto"
+            className="w-70 h-auto"
           />
         </a>
       </div>
